3caaad1765cabb15eee75299d39c551b055bb8be
XodiumLuma <28709874+xodiumluma@users.noreply.github.com>
2023-06-08 00:21:08 +1000

    [learn] update

--- __learn__/source/bin/streaming-data-solution.ts
+++ __learn__/source/bin/streaming-data-solution.ts
@@ -13,3 +13,11 @@ import { KplKdfS3 } from '../patterns/kpl-kds-kda';
 import { MskClientStack } from '../labs/msk-client-setup';
 import { MskClusterStack } from '../labs/msk-cluster-setup';
 import { MskKdaS3 } from '../patterns/msk-kda-s3';
+import { MskLambda } from '../patterns/msk-lamdba';
+import { MskLambdaKdf } from '../patterns/msk-lambda-kdf';
+import { MskLambdaRoleStack } from '../labs/msk-lambda-role';
+import { MskStandalone } from '../patterns/msk-standalone-cluster';
+
+import crypto = require('crypto');
+
+const app = new cdk.App();
